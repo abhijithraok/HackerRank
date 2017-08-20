e339c94843bac73d368405b3646ec1dc88b198d6
Abhijith Rao <abijithraok@gmail.com>
2017-08-21 00:18:00 +0530

    changed algorithm

--- Cracking the Coding Intervie/Arrays_Left Rotation/src/Solution.java
+++ Cracking the Coding Intervie/Arrays_Left Rotation/src/Solution.java
@@ -10,29 +10,12 @@ public class Solution {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
         int k = in.nextInt();
-        int array[] = new int[n];
-        int temp[] = array;
-        for (int a_i = 0; a_i < n; a_i++) {
-            array[a_i] = in.nextInt();
+        int a[] = new int[n];
+        for(int a_i=0; a_i < n; a_i++){
+            a[a_i] = in.nextInt();
         }
-
-        for (int i = 1; i <= k; i++) {
-            temp = rotation(temp);
+        for(int i = 0; i < n; i++) {
+            System.out.print(a[(k+i)%n] + " ");
         }
-        //  System.out.println(Arrays.toString(temp));
-        for (int i = 0; i < temp.length; i++) {
-            System.out.print(temp[i] + " ");
-        }
-
-    }
-
-    public static int[] rotation(int[] grid) {
-        int array[] = new int[grid.length];
-        for (int i = 1; i < grid.length; i++) {
-            array[i - 1] = grid[i];
-        }
-        array[array.length - 1] = grid[0];
-        //  System.out.println(Arrays.toString(array));
-        return array;
     }
 }
