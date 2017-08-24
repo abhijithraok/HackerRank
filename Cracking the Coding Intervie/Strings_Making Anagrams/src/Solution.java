67bd9437be00ad0064b24c001a54d0dd2767c6a6
Abhijith Rao <abijithraok@gmail.com>
2017-08-24 19:05:50 +0530

    changed back to arraylist

--- Cracking the Coding Intervie/Strings_Making Anagrams/src/Solution.java
+++ Cracking the Coding Intervie/Strings_Making Anagrams/src/Solution.java
@@ -6,32 +6,34 @@ import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
+
 public class Solution {
     public static int numberNeeded(String first, String second) {
         int sum = 0;
         int count = 0;
-        LinkedList<Character> list1 = new LinkedList<>();
-        LinkedList<Character> list2 = new LinkedList<>();
-for(int i = 0; i < first.length();i++){
-    list1.add(first.charAt(i));
-}
-for(int i = 0;i< second.length();i++){
-    char currChar = second.charAt(i);
-    int index;
-    if(list1.contains(currChar)){
-        index = list1.indexOf(currChar);
-        list2.add(currChar);
-        list1.remove(index);
-        count +=2;
+        ArrayList<Character> arraylist = new ArrayList<>();
+        ArrayList<Character> arrayList2 = new ArrayList<>();
+        for (int i = 0; i < first.length(); i++) {
+            arraylist.add(first.charAt(i));
+        }
+        for (int i = 0; i < second.length(); i++) {
+            char currChar = second.charAt(i);
+            int index;
+            if (arraylist.contains(currChar)) {
+                index = arraylist.indexOf(currChar);
+                // list2.add(currChar);
+                arrayList2.add(currChar);
+                arraylist.remove(index);
+                count += 2;
 
-    }
-}
+            }
+        }
 
 
         sum = first.length() + second.length();
-     //  System.out.println(list1+"   " +list2 );
-       // System.out.println(count);
-       sum = sum-count;
+        // System.out.println(arrayList2 );
+        // System.out.println(count);
+        sum = sum - count;
         return sum;
     }
 
